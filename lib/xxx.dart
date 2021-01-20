4ae83cc56a6c8f30c611b484301e24bda3329d43
Paolo Cristofoli <superpaolo74@gmail.com>
2021-01-20 19:10:55 +0100

    messo test in main.dart il precedente main.dart di notebook diventa xxx.dart

---
+++ lib/xxx.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'common/utils.dart' as utils;
+import 'screens/recipes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory docsDir = await getApplicationDocumentsDirectory();
+  utils.docsDir = docsDir;
+  runApp(Notebook());
+}
+
+class Notebook extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Notebook"),
+        ),
+        body: Notes(),
+      ),
+    );
+
+  }
+}
