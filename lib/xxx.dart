9f1016533afcf50c94ba0dcc332154a34c3f2945
Paolo Cristofoli <superpaolo74@gmail.com>
2021-01-24 19:34:16 +0100

    devo iniziare a pensare come cambiare dinamicamente il contenuto di appbar

--- lib/xxx.dart
+++ lib/xxx.dart
@@ -4,6 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'common/utils.dart' as utils;
 import 'screens/recipes.dart';
 import 'package:myrecipes_app/models/recipes_model.dart';
+import 'package:provider/provider.dart';
 
 
 void main() async {
@@ -16,26 +17,37 @@ void main() async {
 class Notebook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
+    return ChangeNotifierProvider.value(
+        value: recipesModel,
+        child:
+      MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Notebook"),
           actions: <Widget>[
-            if (recipesModel.stackIndex==0) //da implementare il funzionamento, non si aggiorna la appbar
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () {
-                print("add recipe");
-                recipesModel.recipeBeingEdited = Recipe();
-                recipesModel.setStackIndex(1);
+            //if (recipesModel.stackIndex==0) //da implementare il funzionamento, non si aggiorna la appbar
+
+            Consumer<RecipesModel>(
+              builder:(context,recipesModel,child) {return
+                IndexedStack(index: recipesModel.stackIndex,children:[
+                 IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    print("add recipe");
+                    recipesModel.recipeBeingEdited = Recipe();
+                    recipesModel.setStackIndex(1);
+                  },
+                 ),
+                 Container() //blanck container because I don't need buttons on recipe edit/new
+                ]);
               },
             ),
+
           ],
         ),
         body: Recipes(),
       ),
-    );
+    ),);
 
   }
 }
