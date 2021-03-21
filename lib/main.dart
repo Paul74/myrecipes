dc71f0cb9a49ff8e406ae277575cf423f6a2273c
Paolo Cristofoli <superpaolo74@gmail.com>
2021-03-21 18:25:03 +0100

    cleaning up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,3 @@
-/// Flutter code sample for ListTile
-
-// Here is an example of a custom list item that resembles a Youtube related
-// video list item created with [Expanded] and [Container] widgets.
-//
-// ![Custom list item a](https://flutter.github.io/assets-for-api-docs/assets/widgets/custom_list_item_a.png)
-
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:myrecipes_app/app.dart';
